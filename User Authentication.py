abff894f2c010d725a53df0bab4703564571a2cd
Samaycr <148320560+Samaycr@users.noreply.github.com>
2023-10-18 13:24:49 +0530

    Update User Authentication.py

--- User Authentication.py
+++ User Authentication.py
@@ -3,7 +3,7 @@ from flask_pymongo import PyMongo
 from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
-app.config['MONGO_URI'] = 'mongodb://localhost:27017/your_database_name'
+app.config['MONGO_URI'] = 'YOUR_MONGO_URL'
 mongo = PyMongo(app)
 bcrypt = Bcrypt(app)
 
